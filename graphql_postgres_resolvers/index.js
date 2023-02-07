f05b25788fcb812585781b875b038979e74ebf36
simonsayz13 <simonsayz13@gmail.com>
2023-02-07 16:33:12 +0000

    trying without connectionString

--- graphql_postgres_resolvers/index.js
+++ graphql_postgres_resolvers/index.js
@@ -2,9 +2,11 @@ const { url } = require("inspector");
 
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  connectionString: 'postgresql://postgres:MhYLAaxKGrvbT0e45y2m@containers-us-west-154.railway.app:6839/railway'
-});
+
+const pool = new Pool();
+// const pool = new Pool({
+//   connectionString: 'postgresql://postgres:MhYLAaxKGrvbT0e45y2m@containers-us-west-154.railway.app:6839/railway'
+// });
 
 module.exports = {
   getUsers: async () => {
