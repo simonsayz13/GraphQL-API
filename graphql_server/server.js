62e5242a7c195206cfbe1f7d3d094ff2780ef508
simonsayz13 <simonsayz13@gmail.com>
2023-02-07 16:18:12 +0000

    commented out mongodb

--- graphql_server/server.js
+++ graphql_server/server.js
@@ -3,14 +3,14 @@ const { graphqlHTTP } = require("express-graphql");
 const exerciseSchema = require("../graphql_schema");
 const gqlResolver = require("../graphql_resolvers/");
 
-const psqlResolver = require("../graphql_postgres_resolvers")
+const psqlResolver = require("../graphql_postgres_resolvers");
 const mongoose = require("mongoose");
 
 const app = express();
 
 // export mongodbURI='mongodb+srv://admin:Changeme_123@db1.5y8jmgl.mongodb.net/fitness?retryWrites=true&w=majority'
 
-const uri = process.env.mongodbURI;
+// const uri = process.env.mongodbURI;
 
 app.use(
   "/graphql",
@@ -24,16 +24,22 @@ app.use(
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
-mongoose
-  .connect(uri, options)
-  .then(() =>
-    app.listen(
-      8080,
-      console.log("GraphQL API Server is running on localhost:8080")
-    )
-  )
-  .catch((error) => {
-    throw error;
-  });
+app.listen(
+  8080,
+  console.log("GraphQL API Server is running on localhost:8080")
+)
+
+
+// mongoose
+//   .connect(uri, options)
+//   .then(() =>
+//     app.listen(
+//       8080,
+//       console.log("GraphQL API Server is running on localhost:8080")
+//     )
+//   )
+//   .catch((error) => {
+//     throw error;
+//   });
